6f26f5693f38b0c26297382f2693708b10ce9253
Derek Peacock <derek.peacock@yahoo.com>
2020-12-14 17:47:38 +0000

    Add project files.

---
+++ ConsoleAppProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleAppProject
+{
+    /// <summary>
+    /// The main method in this class is called first
+    /// when the application is started.  It will be used
+    /// to starts Apps 01 to 05 for CO453 CW1A
+    /// 
+    /// This Project has been modified by:
+    /// Derek Peacock 14/12/2020
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
