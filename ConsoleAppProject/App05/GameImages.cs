8b335a62e13f3742617d85bf26d535d3a1819a74
Derek Peacock <derek.peacock@yahoo.com>
2020-12-16 20:31:41 +0000

    Added draw RPS images

---
+++ ConsoleAppProject/App05/GameImages.cs
@@ -0,0 +1,175 @@
+﻿using System;
+
+namespace ConsoleAppProject.App05
+{
+    /// <summary>
+    /// This class has a set of method that will draw out
+    /// some useful game images using textual characters
+    /// for Rock-Paper-Scissors game
+    /// </summary>
+    /// <author>
+    /// Unkown ??
+    /// </author>
+    public static class GameImages
+    {
+        /// <summary>
+        /// *******************************************************
+        /// 
+        /// *******************************************************
+        /// </summary>
+        public static void DrawScissors(int x, int y)
+        {
+            Console.SetCursorPosition(x, y++);   // set start position then increment y to move down
+            Console.Write("     \\            /");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("      \\          /");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("       \\        /");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("        \\      /");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("         \\    /");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("          \\  /");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("           **");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("          /  \\");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("    (----/    \\----)");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("     \\  /      \\  /");
+            Console.SetCursorPosition(x, y);
+            Console.Write("      ==        ==");
+            Console.WriteLine("\n\n");
+        }
+
+        /// <summary>
+        /// *******************************************************
+        /// 
+        /// *******************************************************
+        /// </summary>
+        public static void DrawRock(int x, int y)
+        {
+            Console.SetCursorPosition(x, y++);   // set start position then increment y to move down
+            Console.Write("                 ___---___     ");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("              .--         --.    ");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("           ./   ()       .-. \\.   ");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("           /   o    .   (   )  \\  ");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("          / .            '-'    \\  ");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("         /     ()   ()           \\ ");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("        |    o           ()       | ");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("        |      .--.           O   | ");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("         \\ .  |    |              |  ");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("          \\   `.__.'     o   .   /    ");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("           `\\  o    ()         /'    ");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("              `--___    ___--'    ");
+            Console.SetCursorPosition(x, y);
+            Console.Write("                     ---         ");
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// *******************************************************
+        /// 
+        /// *******************************************************
+        /// </summary>
+        public static void DrawPaper(int x, int y)
+        {
+            Console.SetCursorPosition(x, y++);    // set start position then increment y to move down
+            Console.Write("      .--.------------------.");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("     /      \\  \\ \\ \\ \\ \\ \\ \\ \\");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("    /   OOO  \\                |");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("   |   OOOO   || A N D R E X | |");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("   |   OOOO   |                |");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("    \\   OOO   /                /");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("     \\      // / / / / / / / //");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("       `--'-|| | | | | | | | |");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("             \\                \\");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("              \\                \\");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("               \\                \\");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("                \\ \\ \\ \\ \\ \\ \\ \\ \\\\");
+            Console.SetCursorPosition(x, y);
+            Console.Write("                 \\________________\\");
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// *******************************************************
+        /// This will draw a smile
+        /// *******************************************************
+        /// </summary>
+        public static void DrawSmile()
+        {
+            Console.WriteLine("\n                    .-\"\"\"\"-.-\"\"\"\"-. ");
+            Console.WriteLine("               _.'`               `'._   ");
+            Console.WriteLine("            .-'  __..,.___.___.,..__  '-.   ");
+            Console.WriteLine("           '-.-;` |  |    |    |  | `;-.-'   ");
+            Console.WriteLine("            \\'-\\_/\\__|    |    |__/\\_/-'/   ");
+            Console.WriteLine("             \\, _     '---'---'     _ ,/   ");
+            Console.WriteLine("              \\'./`'.--.--.--,--.'`\\.'/   ");
+            Console.WriteLine("               \\ `'-;__|__|__|__;-'` /   ");
+            Console.WriteLine("                '.                 .'   ");
+            Console.WriteLine("                 `'-....---....-'`    ");
+        }
+
+        /// <summary>
+        /// *******************************************************
+        /// This will draw an image of ThumbsUp on the console
+        /// *******************************************************
+        /// </summary>
+        public static void DrawThumbsUp()
+        {
+            Console.WriteLine();
+            Console.WriteLine("       _ ");
+            Console.WriteLine("      ( ((  ");
+            Console.WriteLine("       \\ =\\   ");
+            Console.WriteLine("      __\\_ `-\\   ");
+            Console.WriteLine("     (____))(  \\-----  ");
+            Console.WriteLine("     (____)) _    ");
+            Console.WriteLine("     (____))   ");
+            Console.WriteLine("     (____))____/-----  ");
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// *******************************************************
+        /// This will draw an image of ThumbsDown on the console
+        /// *******************************************************
+        /// </summary>
+        public static void DrawThumbsDown()
+        {
+            Console.WriteLine();
+            Console.WriteLine("       ______ ");
+            Console.WriteLine("     ((____  \\-----  ");
+            Console.WriteLine("     ((_____         ");
+            Console.WriteLine("     ((_____      ");
+            Console.WriteLine("     ((____   -----   ");
+            Console.WriteLine("          /  /    ");
+            Console.WriteLine("         (_((     ");
+            Console.WriteLine();
+        }
+    }
+}
