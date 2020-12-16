39c0fe8766c32dc411ac3f6c0b49ee53462f325b
Derek Peacock <derek.peacock@yahoo.com>
2020-12-16 20:23:29 +0000

    Added a distance unit enumeration

---
+++ ConsoleAppProject/App01/DistanceUnits.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleAppProject.App01
+{
+    /// <summary>
+    /// List of units used to measure distance
+    /// </summary>
+    public enum DistanceUnits
+    {
+        NoUnit,
+        Feet,
+        Metres,
+        Kilometres,
+        Miles
+    }
+}
