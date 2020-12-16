307e782938e9e5791e6d1bc217c75db99a4b1845
Derek Peacock <derek.peacock@yahoo.com>
2020-12-16 19:27:04 +0000

    Added an empty class for App01

---
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleAppProject.App01
+{
+    /// <summary>
+    /// Please describe the main features of this App
+    /// </summary>
+    /// <author>
+    /// Student Name
+    /// </author>
+    public class DistanceConverter
+    {
+    }
+}
