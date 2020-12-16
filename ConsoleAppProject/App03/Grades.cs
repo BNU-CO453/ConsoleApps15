5e15432a42c221870e7d8e7031cd915050a686bf
Derek Peacock <derek.peacock@yahoo.com>
2020-12-16 20:19:02 +0000

    Added a Grades enumeration

---
+++ ConsoleAppProject/App03/Grades.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleAppProject.App03
+{
+    /// <summary>
+    /// Grade A is First Class   : 70 - 100
+    /// Grade B is Upper Second  : 60 - 69
+    /// Grade C is Lower Second  : 50 - 59
+    /// Grade D is Third Class   : 40 - 49
+    /// Grade F is Fail          :  0 - 39
+    /// </summary>
+    public enum Grades
+    {
+        F, D, C, B, A
+    }
+}
