9083cf06adeb647623e5bd127d8c429d98593871
Derek Peacock <derek.peacock@yahoo.com>
2020-12-26 10:38:57 +0000

    Added empty methods and attributes

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -8,5 +8,31 @@
     /// </author>
     public class DistanceConverter
     {
+        private double miles;
+        private double feet;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Run()
+        {
+
+        }
+
+        /// <summary>
+        /// Prompt the user to enter the distance in miles
+        /// Input the miles as a double number.
+        /// </summary>
+        private void InputMiles()
+        {
+        }
+
+        private void CalculateFeet()
+        {
+        }
+
+        private void OutputFeet()
+        {
+        }
     }
 }
