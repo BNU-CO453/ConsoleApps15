e36048c009308a5e004fe4a763d4d875241ceca1
Derek Peacock <derek.peacock@yahoo.com>
2020-12-26 10:54:59 +0000

    Added a converter to the program class

--- ConsoleAppProject/Program.cs
+++ ConsoleAppProject/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsoleAppProject.App01;
+using System;
 
 namespace ConsoleAppProject
 {
@@ -19,6 +20,9 @@ namespace ConsoleAppProject
             Console.WriteLine("BNU CO453 Applications Programming 2020-2021!");
             Console.WriteLine();
             Console.Beep();
+
+            DistanceConverter converter = new DistanceConverter();
+            converter.Run();
         }
     }
 }
