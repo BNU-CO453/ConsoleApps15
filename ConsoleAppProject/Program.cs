98abae7a12e3c5a2ab5cb114880b3590d67f59f1
Derek Peacock <derek.peacock@yahoo.com>
2022-02-05 20:15:13 +0000

    Updated the modify date

--- ConsoleAppProject/Program.cs
+++ ConsoleAppProject/Program.cs
@@ -7,10 +7,10 @@ namespace ConsoleAppProject
     /// <summary>
     /// The main method in this class is called first
     /// when the application is started.  It will be used
-    /// to start Apps 01 to 05 for CO453 CW1
+    /// to start App01 to App05 for CO453 CW1
     /// 
     /// This Project has been modified by:
-    /// Derek Peacock 14/12/2020
+    /// Derek Peacock 05/02/2022
     /// </summary>
     public static class Program
     {
